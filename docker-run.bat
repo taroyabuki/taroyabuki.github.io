4df8f643449f571e1ec88ec8ce17a9ad24cc452a
Taro YABUKI <taroyabuki@users.noreply.github.com>
2019-11-26 23:50:53 +0900

    辻真吾『Pythonで学ぶアルゴリズムとデータ構造』

--- docker-run.bat
+++
@@ -1,1 +0,0 @@
-docker run -it -p 4000:4000 -v C:/Users/yabuki/Dropbox/taroyabuki.github.io:/srv/jekyll --name jekyll jekyll/jekyll bash
---
+++ docker-run.sh
@@ -0,0 +1,3 @@
+docker run -it -p 4000:4000 -v ${PWD}:/srv/jekyll --name jekyll jekyll/jekyll bash
+#echo -e "151.101.64.70 rubygems.org\n151.101.64.70 api.rubygems.org" >> /etc/hosts
+#jekyll serve
